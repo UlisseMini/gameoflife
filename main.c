73253888f31052a34c2a2bb0323f06c2906c3b42
Ulisse Mini <ulisse.mini@gmail.com>
2021-01-24 12:05:18 -0500

    Shorten code, spam comments

--- main.c
+++ main.c
@@ -41,35 +41,31 @@ void my_free(void* ptr, char* file, int line) {
 
 /* The program */
 
+/* How big the grid should be, 40 looks good on my split tmux terminal. */
 #define N 40
 
-typedef enum Cell { 
-  DEAD=0, ALIVE=1
-} E_Cell;
+/* A Cell can be alive or dead */
+typedef enum Cell { DEAD=0, ALIVE=1 } E_Cell;
 
-
+/* Print the world in ascii */
 void print_world(E_Cell** world) {
   for (int x = 0; x < N; x++) {
     for (int y = 0; y < N; y++) {
       E_Cell cell = world[y][x];
-      char c = '?';
-      switch (cell) {
-        case ALIVE:     c = '#'; break;
-        case DEAD:      c = '-'; break;
-      }
+      char c = cell == ALIVE ? '#' : cell == DEAD ? '-' : '?';
       printf("%c ", c);
     }
     printf("\n");
   }
 }
 
+/* free() the world, column by column */
 void free_world(E_Cell** world) {
-  for (int i = 0; i < N; i++) {
-    free(world[i]);
-  }
+  for (int i = 0; i < N; i++) { free(world[i]); }
   free(world);
 }
 
+/* malloc a new world, and its columns (NOTE: Not zeroed!) */
 E_Cell** new_world() {
   E_Cell** world = malloc(N * sizeof(E_Cell*));
   for (int i = 0; i < N; i++) {
@@ -103,6 +99,7 @@ int neighbors(E_Cell** world, int y, int x, E_Cell type) {
   return n;
 }
 
+/* Make a step in the world, n_world is overwritten with the new world. */
 void step_world(E_Cell** world, E_Cell** n_world) {
   for (int x = 0; x < N; x++) {
     for (int y = 0; y < N; y++) {
@@ -136,10 +133,12 @@ int main() {
   world[21][19] = ALIVE;
   world[21][18] = ALIVE;
 
+  /* Make sure neighbors is working correctly */
   assert(neighbors(world, 21, 18, ALIVE) == 1);
   assert(neighbors(world, 21, 19, ALIVE) == 3);
   assert(neighbors(world, 20, 19, ALIVE) == 5);
 
+  /* Alternate between overwriting world, and n_world */
   for (int steps = 0; steps < 80; steps++) {
     if (steps % 2 == 0) { 
       print_world(world);
@@ -148,11 +147,10 @@ int main() {
       print_world(n_world);
       step_world(n_world, world);
     }
-    printf("\x1b[%dA", N);
-    usleep(1000*100);
+    printf("\x1b[%dA", N); /* Escape code for up N lines */
+    usleep(1000*100); /* Sleep for 100000microseconds = 100ms */
   }
   print_world(n_world);
-
 
   free_world(world);
   free_world(n_world);
